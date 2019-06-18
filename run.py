1d213a91a35ee37a08edb6d241724cebbc5a4d76
Tobias Schubotz <tobias.schubotz@gmail.com>
2019-06-18 11:48:56 +0200

    Fix html links

--- run.py
+++ run.py
@@ -21,23 +21,28 @@ output = '_Last updated: {}_\n\n'.format(datetime.now())
 
 for repo_name in REPOS:
     repo = g.get_repo(repo_name)
+    
 
-    output += '# [{}]({})\n\n'.format(repo.name, repo.url)
+    output += '# [{}]({})\n\n'.format(repo.name, repo.html_url)
 
     open_milestones = repo.get_milestones(state='open')
     
     for milestone in open_milestones:
+        
+        # Milestones don't have .html_url, so creating it manually:
+        milestone_html_url = '{}/milestone/{}'.format(repo.html_url, milestone.number)
+
         output += '### [{}]({}) {}/{} issues ({:.0f}%)\n\n'.format(
             milestone.title, 
-            milestone.url,
+            milestone_html_url,
             milestone.closed_issues,
             milestone.open_issues + milestone.closed_issues,
             100 * milestone.closed_issues / (milestone.open_issues + milestone.closed_issues))
         
         for issue in repo.get_issues(milestone=milestone, state='closed'):
-            output += '- ✅ [{}]({})\n'.format(issue.title, issue.url)
+            output += '- ✅ [{}]({})\n'.format(issue.title, issue.html_url)
         for issue in repo.get_issues(milestone=milestone, state='open'):
-            output += '- 🏗 [{}]({})\n'.format(issue.title, issue.url)
+            output += '- 🏗 [{}]({})\n'.format(issue.title, issue.html_url)
     
     if open_milestones.totalCount == 0:
         output += 'Currently no milestones opens.\n\n'
